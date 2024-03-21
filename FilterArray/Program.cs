a87d39b761e31f7bc16163c26533426ef53c59a6
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-03-21 11:37:40 +0300

    Добавьте файлы проекта.

---
+++ FilterArray/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FilterArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример использования метода
+            int[] originalArray = { 1, 2, 6, -1, 88, 7, 6 };
+            int[] filterArray = { 6, 88, 7 };
+
+            int[] filteredArray = FilterArray(originalArray, filterArray);
+
+            Console.WriteLine("Результат работы метода:");
+            foreach (int num in filteredArray)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        static int[] FilterArray(int[] originalArray, int[] filterArray)
+        {
+            return originalArray.Except(filterArray).ToArray();
+        }
+    }
+}
